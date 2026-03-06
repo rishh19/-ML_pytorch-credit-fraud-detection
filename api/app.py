593e2e5b19928eed73eef87cfd678519fb7dac5d
rishh19 <235rishavstar@gmail.com>
2026-03-06 21:04:22 +0530

    Add CORS middleware to FastAPI application

--- api/app.py
+++ api/app.py
@@ -1,10 +1,17 @@
+from fastapi.middleware.cors import CORSMiddleware
 import torch
 import torch.nn as nn
 from fastapi import FastAPI
 import numpy as np
 
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 class FraudDetectionModel(nn.Module):
 
     def __init__(self):
